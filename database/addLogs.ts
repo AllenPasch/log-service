0c8828ab190516631d5d1567d776a7be862ce5e3
Allen Pasch <github@irishcousin.com>
2025-05-28 00:10:57 +0200

    feat: insert logs into the sqlite table

---
+++ database/addLogs.ts
@@ -0,0 +1,31 @@
+import { type Database } from "sqlite";
+import { type Database as DatabaseV3, type Statement } from "sqlite3";
+
+import { type LogType } from "../schema/Log";
+
+export const addLogs = async (
+  db: Database<DatabaseV3, Statement>,
+  logs: readonly LogType[]
+) =>
+  await Promise.all(
+    logs.map(async ({ timestamp, source, severity, message }) => {
+      const timestampMs = new Date(timestamp).getTime();
+
+      const statement = await db.prepare(
+        `INSERT INTO log (
+          timestamp_ms,
+          source,
+          severity,
+          message
+        ) VALUES (
+          ?,
+          ?,
+          ?,
+          ?
+        )`,
+        [timestampMs, source, severity, message]
+      );
+
+      await statement.run();
+    })
+  );
